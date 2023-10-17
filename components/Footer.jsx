66d39ff191ccb757f0e91d03b6243b06b1633b2c
rahul <rahul.baradol.14@gmail.com>
2023-10-17 17:05:13 +0530

    Fixed a typo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -44,7 +44,7 @@ function Footer() {
         <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
           <div className="flex items-center justify-between flex-wrap gap-5">
             <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-              Register for Code Of Honor
+              Register for Code Of Honour
             </h4>
             <button
               type="button"
