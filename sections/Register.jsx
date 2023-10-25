43d8ab730d3f68fb81a16d737e35242a2c22f186
rahul <rahul.baradol.14@gmail.com>
2023-10-25 16:48:22 +0530

    Semester input change to 1,3,5,7

--- sections/Register.jsx
+++ sections/Register.jsx
@@ -546,10 +546,10 @@ export default function Registrations() {
                             handleTextChange(data.id, e.target.value, 'semester')
                           }
                         >
-                          <option value={2}>2</option>
-                          <option value={4}>4</option>
-                          <option value={6}>6</option>
-                          <option value={8}>8</option>
+                          <option value={1}>1</option>
+                          <option value={3}>3</option>
+                          <option value={5}>5</option>
+                          <option value={7}>7</option>
                         </select>
                       </label>
   
